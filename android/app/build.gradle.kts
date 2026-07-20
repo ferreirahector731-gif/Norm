13d7886fb2c7f0b689128a931637381f027a521a
ferreirahector731-gif <ferreirahector731@gmail.com>
2026-07-20 12:23:09 -0300

    fix(core): congelar Flutter en 3.24.5 y subir Android compileSdk a 35 para estabilidad total

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.nota_ia_app"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 35
     ndkVersion = "27.0.12077973"
 
     compileOptions {
