82c7cb5ea127a4c2ae43907f0dc0f4c3675f80a3
ferreirahector731-gif <ferreirahector731@gmail.com>
2026-07-20 21:21:04 -0300

    fix: REEMPLAZO COMPLETO de build.gradle.kts, zip macOS sin -i Contents, docs/ incluido

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.7.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.20")
     }
 }
 
@@ -16,28 +16,24 @@ allprojects {
     }
 }
 
-rootProject.buildDir = "../build"
+rootProject.buildDir = file("../build")
 subprojects {
-    project.buildDir = "${rootProject.buildDir}/${project.name}"
+    project.buildDir = file("${rootProject.buildDir}/${project.name}")
 }
 subprojects {
     project.evaluationDependsOn(":app")
 }
 
-tasks.register("clean", Delete) {
-    delete rootProject.buildDir
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
 }
 
 // PATCH: Forzar namespace en isar_flutter_libs
 subprojects {
     afterEvaluate {
         if (project.name == "isar_flutter_libs") {
-            // Usar LibraryExtension para evitar el error de android no encontrado
-            if (project.extensions.findByName("android") != null) {
-                project.extensions.findByType(com.android.build.gradle.LibraryExtension::class.java)?.apply {
-                    namespace = "dev.isar.isar_flutter_libs"
-                }
-            }
+            val libExtension = project.extensions.findByName("android") as? com.android.build.gradle.LibraryExtension
+            libExtension?.namespace = "dev.isar.isar_flutter_libs"
         }
     }
 }
