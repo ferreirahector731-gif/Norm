07b864febc70010130864114602432158369ed4a
ferreirahector731-gif <ferreirahector731@gmail.com>
2026-07-21 17:49:42 -0300

    fix: eliminar afterEvaluate redundante que causaba timing error en Gradle

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -28,20 +28,12 @@ tasks.register<Delete>("clean") {
     delete(rootProject.buildDir)
 }
 
-// PATCH: Forzar namespace y compileSdk en isar_flutter_libs y todos los submódulos Android
+// PATCH: Forzar namespace y compileSdk en isar_flutter_libs
 subprojects {
     plugins.withId("com.android.library") {
         extensions.configure<com.android.build.gradle.LibraryExtension> {
             namespace = "dev.isar.isar_flutter_libs"
             compileSdk = 36
-        }
-    }
-    afterEvaluate {
-        if (project.hasProperty("android")) {
-            val androidExt = project.extensions.findByName("android")
-            if (androidExt is com.android.build.gradle.BaseExtension) {
-                androidExt.compileSdkVersion(36)
-            }
         }
     }
 }
